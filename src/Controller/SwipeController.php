9a84ccf8a5a0e24a1f567ca3423892a6da85507d
Gumbraise <vikiclashonyt@gmail.com>
2022-07-20 17:15:58 +0200

    d

---
+++ src/Controller/SwipeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SwipeController extends AbstractController
+{
+    /**
+     * @Route("/swipe", name="app_swipe")
+     */
+    public function index(): Response
+    {
+        return $this->render('swipe/index.html.twig', [
+            'controller_name' => 'SwipeController',
+        ]);
+    }
+}
