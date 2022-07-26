9eece4437c2e6cbbadc3a5fd953815e78635866a
Gumbraise <vikiclashonyt@gmail.com>
2022-07-26 12:03:38 +0200

    Update swipe.js

--- assets/swipe.js
+++ assets/swipe.js
@@ -11,10 +11,12 @@ let palette = require('image-palette');
 let pixels = require('image-pixels');
 
 async function ipalette() {
-    let {colors} = palette(await pixels(require(`./images/wyssual_logo.png`)), 3);
+    let {colors} = palette(await pixels(require(`./images/swipeup.png`)), 3);
+
+    console.log(colors)
 
     let dom = document.getElementById('spotlight');
-    dom.style.backgroundImage = `linear-gradient(45deg, rgba(${colors[0][0]},${colors[0][1]},${colors[0][2]},${colors[0][3]}), rgba(${colors[1][0]},${colors[1][1]},${colors[1][2]},${colors[1][3]}) 50%, rgba(${colors[2][0]},${colors[2][1]},${colors[2][2]},${colors[2][3]}))`;
+    dom.style.backgroundImage = `linear-gradient(45deg, rgba(${colors[2][0]},${colors[2][1]},${colors[2][2]},${colors[2][3]}), rgba(${colors[1][0]},${colors[1][1]},${colors[1][2]},${colors[1][3]}) 50%, rgba(${colors[0][0]},${colors[0][1]},${colors[0][2]},${colors[0][3]}))`;
     // dom.style.backgroundImage = "linear-gradient(45deg, #00dc82, #36e4da 50%, #0047e1)";
     return colors;
 }
