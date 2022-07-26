3c115680ccf68d91424cda2015e2a20f72526c29
Gumbraise <vikiclashonyt@gmail.com>
2022-07-26 11:17:19 +0200

    Update swipe.js

--- assets/swipe.js
+++ assets/swipe.js
@@ -8,7 +8,6 @@ async function ipalette() {
     let {colors} = palette(await pixels(require(`./images/wyssual_logo.png`)), 3);
 
     let dom = document.getElementById('spotlight');
-    console.log(colors)
     dom.style.backgroundImage = `linear-gradient(45deg, rgba(${colors[0][0]},${colors[0][1]},${colors[0][2]},${colors[0][3]}), rgba(${colors[1][0]},${colors[1][1]},${colors[1][2]},${colors[1][3]}) 50%, rgba(${colors[2][0]},${colors[2][1]},${colors[2][2]},${colors[2][3]}))`;
     // dom.style.backgroundImage = "linear-gradient(45deg, #00dc82, #36e4da 50%, #0047e1)";
     return colors;
