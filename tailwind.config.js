c6579cb953d3bb710654069d89fd558a6c434d75
Gumbraise <vikiclashonyt@gmail.com>
2022-08-19 10:06:06 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       fontFamily: {
         'swipe': ['"Reem Kufi"', 'sans-serif'],
       },
+      colors: {
+        'swipe': {
+          '100': "#05fefc",
+          '200': "#08e8f8",
+          '300': "#0bd2f3",
+          '400': "#0ebcef",
+          '500': "#11a6ea",
+          '600': "#1490e6",
+          '700': "#177ae1",
+          '800': "#1a64dd",
+          '900': "#1d4ed8",
+        }
+      }
     },
   },
   plugins: [],
