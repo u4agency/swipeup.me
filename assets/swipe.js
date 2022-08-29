4667d89e7d8df79ba0dd4795965e50ed42c5b060
Gumbraise <vikiclashonyt@gmail.com>
2022-08-29 14:15:39 +0200

    css to scss

--- assets/swipe.js
+++ assets/swipe.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/swiper-bundle.min.css';
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
