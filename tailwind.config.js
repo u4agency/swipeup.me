66b41a5bfd6d97d79936b4b5523906c3825b0957
Gumbraise <vikiclashonyt@gmail.com>
2023-02-09 11:02:41 +0100

    improved homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./assets/**/*.js",
-        'templates/**/*.html.twig',
-    ],
-    theme: {
+    content: ["./assets/**/*.js", 'templates/**/*.html.twig', 'templates/*.html.twig', 'src/**/*.php',], theme: {
         extend: {
             inset: {
-                '1/5': '20%',
-                '2/5': '40%',
-                '3/5': '60%',
-                '4/5': '80%',
-            },
-            minHeight: {
+                '1/5': '20%', '2/5': '40%', '3/5': '60%', '4/5': '80%',
+            }, minHeight: {
                 'hd': '720px',
-            },
-            maxHeight: {
+            }, maxHeight: {
                 'fhd': '1920px',
-            },
-            fontFamily: {
+            }, fontFamily: {
                 'swipe': ['"Reem Kufi"', 'sans-serif'],
-            },
-            colors: {
+            }, colors: {
                 'swipe': {
                     '100': "#05fefc",
                     '200': "#08e8f8",
@@ -33,48 +22,40 @@ module.exports = {
                     '800': "#1a64dd",
                     '900': "#1d4ed8",
                 }
-            },
-            transitionTimingFunction: {
+            }, transitionTimingFunction: {
                 'gradient': 'cubic-bezier(.1, 0, .9, 1)',
-            },
-            keyframes: {
-                firstGradient: {
+            }, keyframes: {
+                'scroll': {
+                    '0%': {transform: 'translate(0)'}, '100%': {transform: 'translate(-50%)'}
+                }, firstGradient: {
                     '0%': {transform: 'translateY(-50%) translateX(-50%) rotate(-20deg) translateX(20%)'},
                     '25%': {transform: 'translateY(-50%) translateX(-50%) skew(-15deg, -15deg) rotate(80deg) translateX(30%)'},
                     '50%': {transform: 'translateY(-50%) translateX(-50%) rotate(180deg) translateX(25%)'},
                     '75%': {transform: 'translateY(-50%) translateX(-50%) skew(15deg, 15deg) rotate(240deg) translateX(15%)'},
                     '100%': {transform: 'translateY(-50%) translateX(-50%) rotate(340deg) translateX(20%)'},
-                },
-                secondGradient: {
+                }, secondGradient: {
                     '0%': {transform: 'translateY(-50%) translateX(-50%) rotate(40deg) translateX(-20%)'},
                     '25%': {transform: 'translateY(-50%) translateX(-50%) skew(15deg, 15deg) rotate(110deg) translateX(-5%)'},
                     '50%': {transform: 'translateY(-50%) translateX(-50%) rotate(210deg) translateX(-35%)'},
                     '75%': {transform: 'translateY(-50%) translateX(-50%) skew(-15deg, -15deg) rotate(300deg) translateX(-10%)'},
                     '100%': {transform: 'translateY(-50%) translateX(-50%) rotate(400deg) translateX(-20%)'},
-                },
-                thirdGradient: {
+                }, thirdGradient: {
                     '0%': {transform: 'translateY(-50%) translateX(-50%) translateX(-15%) translateY(10%)'},
                     '20%': {transform: 'translateY(-50%) translateX(-50%) translateX(20%) translateY(-30%)'},
                     '40%': {transform: 'translateY(-50%) translateX(-50%) translateX(-25%) translateY(-15%)'},
                     '60%': {transform: 'translateY(-50%) translateX(-50%) translateX(30%) translateY(20%)'},
                     '80%': {transform: 'translateY(-50%) translateX(-50%) translateX(5%) translateY(35%)'},
                     '100%': {transform: 'translateY(-50%) translateX(-50%) translateX(-15%) translateY(10%)'},
+                }, spotlight: {
+                    '0%': {opacity: 1}, '50%': {opacity: 1}, '100%': {opacity: 1},
                 },
-                spotlight: {
-                    '0%': {opacity: 1},
-                    '50%': {opacity: 1},
-                    '100%': {opacity: 1},
-                },
-            },
-            animation: {
+            }, animation: {
                 'gradient-1': 'firstGradient 11s ease infinite',
                 'gradient-2': 'secondGradient 11s ease infinite reverse',
                 'gradient-3': 'thirdGradient 11s ease infinite',
                 'spotlight': 'spotlight 10s infinite',
+                'scroll': 'scroll 60s linear infinite',
             },
         },
-    },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
+    }, plugins: [require('@tailwindcss/forms'),],
 }
