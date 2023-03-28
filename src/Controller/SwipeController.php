d5d30d28db13cb8d8a604e1b0a96968a812bc03b
Gumbraise <vikiclashonyt@gmail.com>
2023-03-28 14:40:10 +0200

    Added newsletter popup

--- src/Controller/SwipeController.php
+++ src/Controller/SwipeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
 use App\Entity\Swipe;
+use App\Form\NewsletterType;
 use App\Repository\SwipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,11 +27,35 @@ class SwipeController extends AbstractController
     }
 
     #[Route('@{slug}', name: 'app_swipe_single', priority: -1)]
-    public function singleSwipe(Swipe $swipe): Response
+    public function singleSwipe(
+        Swipe                  $swipe,
+        EntityManagerInterface $entityManager,
+        Request                $request
+    ): Response
     {
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        if ($swipe->isHomepageDisplay()) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted()) {
+                if ($form->isValid()) {
+                    $newsletter->setSource($request->attributes->get('_route'));
+                    try {
+                        $entityManager->persist($newsletter);
+                        $entityManager->flush();
+
+                        $this->addFlash('success', "Vous êtes bien inscrit à la file d'attente !");
+                    } catch (\Exception $exception) {
+                        $this->addFlash('danger', "Vous êtes déjà en file d'attente !");
+                    }
+                }
+            }
+        }
+
         return $this->render('swipe/single.html.twig', [
             'controller_name' => 'SwipeController',
-            'swipe' => $swipe
+            'swipe' => $swipe,
+            'newsletterForm' => $form
         ]);
     }
 
