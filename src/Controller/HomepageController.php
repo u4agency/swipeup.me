117dfca29c8c9fe862ccd5e7ee69938a64ef61b5
Gumbraise <vikiclashonyt@gmail.com>
2023-03-28 12:21:12 +0200

    Added Newsletter on Homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
 use App\Repository\SwipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,12 +15,32 @@ class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function index(
-        SwipeRepository $swipeRepository,
+        SwipeRepository        $swipeRepository,
+        EntityManagerInterface $entityManager,
+        Request                $request
     ): Response
     {
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $newsletter->setSource($request->attributes->get('_route'));
+                try {
+                    $entityManager->persist($newsletter);
+                    $entityManager->flush();
+
+                    $this->addFlash('success', "Vous êtes bien inscrit à la file d'attente !");
+                } catch (\Exception $exception) {
+                    $this->addFlash('danger', "Vous êtes déjà en file d'attente !");
+                }
+            }
+        }
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
             'swipes' => $swipeRepository->findAll(),
+            'newsletterForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsletterRepository::class)]
+class Newsletter
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $source = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+
+    public function setSource(string $source): self
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+}
