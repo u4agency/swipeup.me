7ec58aaee7b9a936af277a5cf001f98c0efe40fb
Gumbraise <vikiclashonyt@gmail.com>
2023-04-06 18:11:01 +0200

    yanr add postcss-100vh-fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
+    plugins: [require('postcss-100vh-fix'), require('autoprefixer'), require('tailwindcss')]
 };
