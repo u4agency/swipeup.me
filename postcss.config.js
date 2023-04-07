6daa2d099a2a780b20cd36aadd3bf910b9faeb63
Gumbraise <vikiclashonyt@gmail.com>
2023-04-07 14:32:51 +0200

    Fixed maybe

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('postcss-100vh-fix'),
         require('postcss-viewport-height-correction'),
         require('autoprefixer'),
     ]
