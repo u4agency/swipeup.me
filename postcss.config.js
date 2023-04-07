3e218b733a626882572c77eb8fb72034cdfd0448
Gumbraise <vikiclashonyt@gmail.com>
2023-04-07 11:39:05 +0200

    Fixed height issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     plugins: [
+        require('tailwindcss'),
         require('postcss-100vh-fix'),
         require('postcss-viewport-height-correction'),
         require('autoprefixer'),
-        require('tailwindcss')
     ]
 };
