6ae3016c9690091e15d6a7b17c302cf39172f15c
Gumbraise <vikiclashonyt@gmail.com>
2023-07-31 12:19:09 +0200

    🔧 Configured @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,5 +62,9 @@ module.exports = {
             },
         },
     },
-    plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('flowbite/plugin'),
+    ],
 }
