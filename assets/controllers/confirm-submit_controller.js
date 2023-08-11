74bb8780cd2742b8a7872fba8bfb1d34880eee01
Gumbraise <vikiclashonyt@gmail.com>
2023-08-11 17:04:15 +0200

    ✨ Added ability to remove Swipe

---
+++ assets/controllers/confirm-submit_controller.js
@@ -0,0 +1,45 @@
+import {Controller} from "@hotwired/stimulus";
+import Swal from "sweetalert2";
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    static values = {
+        title: String, text: String, icon: String, confirmationButtonText: String, submitAsync: Boolean
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        Swal.fire({
+            title: this.titleValue || null,
+            text: this.textValue || null,
+            icon: this.iconValue || null,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: this.confirmationButtonTextValue || 'Oui',
+            cancelButtonText: 'Annuler',
+            showLoaderOnConfirm: true,
+            preConfirm: () => {
+                return this.submitForm();
+            }
+        });
+    }
+
+    async submitForm() {
+        if (!this.submitAsyncValue) {
+            this.element.submit();
+
+            return;
+        }
+
+        const response = await fetch(this.element.action, {
+            method: this.element.method,
+            body: new FormData(this.element)
+        });
+
+        this.dispatch('async:submitted', {
+            response,
+        });
+    }
+}
