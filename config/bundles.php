0c2aaf66103e56eddd86bb805facc1d3541fc4d1
Gumbraise <vikiclashonyt@gmail.com>
2023-08-28 15:53:26 +0200

    📦️ Added liip/imagine-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     MarcW\Heroicons\MarcWHeroiconsBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
