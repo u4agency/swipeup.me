b53e077bfa8581bc72629300e50736a3e9c51517
Gumbraise <vikiclashonyt@gmail.com>
2023-11-06 23:47:50 +0100

    ✨ Added createdAt on User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -54,6 +55,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
     private ?string $hostedDomain;
 
+    #[ORM\Column]
+    private ?DateTimeImmutable $createdAt = null;
+
     public function __toString()
     {
         return $this->username ?? $this->email;
@@ -64,6 +68,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->swipeImages = new ArrayCollection();
         $this->swipeUps = new ArrayCollection();
         $this->widgets = new ArrayCollection();
+        $this->createdAt = new DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -277,5 +282,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setFacebookId(?string $facebookId): void
     {
         $this->facebookId = $facebookId;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
     }
 }
