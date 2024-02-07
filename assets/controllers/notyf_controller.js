21c5532e3c32daa33fe47d1167561b432a261074
Gumbraise <vikiclashonyt@gmail.com>
2024-02-08 00:01:52 +0100

    🚑️ Fixed flashes bug #125

--- assets/controllers/notyf_controller.js
+++ assets/controllers/notyf_controller.js
@@ -12,7 +12,9 @@ export default class extends Controller {
 
     connect() {
         if (this.flashesValue && typeof this.flashesValue === "object" && !Array.isArray(this.flashesValue)) {
+            console.log("ok");
             for (const type in this.flashesValue) {
+                console.log(type);
                 this.flashesValue[type].forEach(message => {
                     notyf(type, message);
                 })
